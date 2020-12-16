947ea2e28cca618677719cce9996dc320e36260e
araf-rahman <araf91@gmail.com>
2020-12-16 16:39:44 -0500

    Fix deployment issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/Search.css'
 import Search from './Component/Search';
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(<Search />, document.getElementById('root'));
 
