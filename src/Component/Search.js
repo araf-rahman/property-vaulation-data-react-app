a4cdbdfa608e1d8365991466d99475cafdc4ed6f
araf-rahman <araf91@gmail.com>
2020-12-16 13:45:24 -0500

    Fully funcitonal app

---
+++ src/Component/Search.js
@@ -0,0 +1,80 @@
+  import React, { Component } from "react";
+  import Table from 'react-bootstrap/Table'
+
+  class Search extends Component {
+      state = {
+        searchValue: "",
+        data: [],
+      };
+    
+      handleOnChange = event => {
+        this.setState({ searchValue: event.target.value });
+      };
+    
+      handleSearch = () => {
+        this.getPropertyInfo(this.state.searchValue);
+      };
+    // TODO: Utilize .env package to hide api key
+      getPropertyInfo = searchInput => {
+        var searchUrl = `https://data.cityofnewyork.us/resource/8y4t-faws.json?$$app_token=4Yj9vNOcp0lchNaAOG6JdfbT5&$q=${searchInput}&$order=year DESC`;
+        fetch(searchUrl)
+          .then(response => {
+            return response.json();
+          })
+          .then(jsonData => {
+            this.setState({ taxInfo: jsonData });
+          });
+      };
+    // TODO: Convert this to fragrament 
+      render() {
+        return (
+          <div id="main">
+            <h1>NYC Property Valuation and Assessment Info</h1>
+            <input
+              name="text"
+              type="text"
+              placeholder="Search"
+              onChange={event => this.handleOnChange(event)}
+              value={this.state.searchValue}
+            />
+            <button onClick={this.handleSearch}>Search</button>
+            {this.state.taxInfo ? (
+              <div>
+                <Table striped bordered hove size="sm">
+                <thead>
+                  <tr>
+                    <th>Owner</th>
+                    <th>Property Address</th>
+                    <th>ZipCode</th>
+                    <th>Tax Year</th>
+                    <th>Current Year Final Market Value</th>
+                    <th>Current Year Taxable Value Value</th>
+                    <th>Prior Year Final Market Value</th>
+                    <th>Prior Year Taxable Value Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {this.state.taxInfo.map((data, index) => (
+                    <tr key={index}> 
+                      <td>{data.owner}</td>
+                      <td>{data.housenum_hi} {data.street_name} {data.boro}</td>
+                      <td>{data.zip_code}</td>
+                      <td>{data.year}</td>
+                      <td>${data.finmkttot}</td>
+                      <td>${data.fintrntot}</td>
+                      <td>${data.pymkttot}</td>
+                      <td>${data.pytxbtot}</td>
+                    </tr>
+                ))}
+                </tbody>
+                </Table>
+                </div>
+            ) : (
+              <p>Enter Property Address(Street number and name) to get more info</p>
+            )}
+          </div>
+        );
+      }
+    }
+    
+    export default Search;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/Search.css'
+import Search from './Component/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.render(<Search />, document.getElementById('root'));
+
